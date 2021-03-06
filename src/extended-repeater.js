750c40e0f9ac8379bc8699f0c686d837812a5aaf
beeebeep <YouCanWriteMe@ya.ru>
2021-03-07 01:43:25 +0700

    fix: implement extended-repeater hanoi-tower transform-array what-season

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(
+  str,
+  {
+    repeatTimes = 1,
+    separator = "+",
+    addition = "",
+    additionRepeatTimes = 1,
+    additionSeparator = "|",
+  }
+) {
+  str +=
+    (addition + additionSeparator).repeat(additionRepeatTimes - 1) + addition;
+  return (str + separator).repeat(repeatTimes - 1) + str;
 };
-  
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("Error");
+  }
+  let res = [];
+  let arrLength = arr.length;
+  let discardedIndex = 0;
+  for (let i = 0; i < arrLength; i++) {
+    const e = arr[i];
+    switch (e) {
+      case "--discard-next":
+        discardedIndex = ++i;
+        break;
+      case "--double-next":
+        if (i + 1 < arrLength) {
+          res.push(arr[i + 1]);
+        }
+        break;
+      case "--discard-prev":
+        if (res.length && discardedIndex != i - 1) {
+          discardedIndex = i - 1;
+          res.pop();
+        }
+        break;
+      case "--double-prev":
+        if (i - 1 > 0 && discardedIndex != i - 1) {
+          res.push(arr[i - 1]);
+        }
+        break;
+      default:
+        res.push(e);
+    }
+  }
+  return res;
 };
