35aeac1e2bfe0f8ac287332f6df9cc1626d900ac
beeebeep <YouCanWriteMe@ya.ru>
2021-03-28 05:03:05 +0700

    fix: implement vigere cipher class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+    constructor(direct = true) {
+        this.reverse = !direct;
+    }
+
+    encrypt(message, key) {
+        if (message == undefined || key == undefined) {
+            throw new Error();
+        }
+        key = key.toUpperCase();
+        let pointer = 0;
+        let mapper = (m) => alphabet.includes(m) ? alphabet[(alphabet.indexOf(m) + alphabet.indexOf(key[pointer++ % key.length])) % alphabet.length] : m;
+        let changed = message.toUpperCase().split('').map(mapper);
+        return this.reverse ? changed.reverse().join('') : changed.join('');
+    }
+
+    decrypt(message, key) {
+        if (message == undefined || key == undefined) {
+            throw new Error();
+        }
+        key = key.toUpperCase();
+        let pointer = 0;
+        let mapper = (c) => alphabet.includes(c) ? alphabet[(alphabet.indexOf(c) + alphabet.length - alphabet.indexOf(key[pointer++ % key.length])) % alphabet.length] : c;
+        let changed = message.toUpperCase().split('').map(mapper);
+        return this.reverse ? changed.reverse().join('') : changed.join('');
+    }
 }
 
 module.exports = VigenereCipheringMachine;
+
+    // process(message, key, mapper) {
+    //     this.pointer = 0; ?@!%//
+    //     if (message == undefined || key == undefined) {
+    //         throw new Error();
+    //     }
+    //     key = key.toUpperCase();
+    //     let changed = message.toUpperCase().split('').map(mapper);
+    //     return this.reverse ? changed.reverse().join('') : changed.join('');
+    // }
+
