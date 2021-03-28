68cd535f28c955a7de412b32f0e05e1ed3f1be46
beeebeep <YouCanWriteMe@ya.ru>
2021-03-29 01:38:18 +0700

    refactor: apply DRY principle

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -8,37 +8,22 @@ class VigenereCipheringMachine {
     }
 
     encrypt(message, key) {
-        if (message == undefined || key == undefined) {
-            throw new Error();
-        }
-        key = key.toUpperCase();
-        let pointer = 0;
-        let mapper = (m) => alphabet.includes(m) ? alphabet[(alphabet.indexOf(m) + alphabet.indexOf(key[pointer++ % key.length])) % alphabet.length] : m;
-        let changed = message.toUpperCase().split('').map(mapper);
-        return this.reverse ? changed.reverse().join('') : changed.join('');
+        return this.process(message, key, (m) => alphabet.includes(m) ? alphabet[(alphabet.indexOf(m) + alphabet.indexOf(this.tkey[this.pointer++ % this.tkey.length])) % alphabet.length] : m);
     }
 
     decrypt(message, key) {
+        return this.process(message, key, (c) => alphabet.includes(c) ? alphabet[(alphabet.indexOf(c) + alphabet.length - alphabet.indexOf(this.tkey[this.pointer++ % this.tkey.length])) % alphabet.length] : c);
+    }
+
+    process(message, key, mapper) {
         if (message == undefined || key == undefined) {
             throw new Error();
         }
-        key = key.toUpperCase();
-        let pointer = 0;
-        let mapper = (c) => alphabet.includes(c) ? alphabet[(alphabet.indexOf(c) + alphabet.length - alphabet.indexOf(key[pointer++ % key.length])) % alphabet.length] : c;
+        this.pointer = 0;
+        this.tkey = key.toUpperCase();
         let changed = message.toUpperCase().split('').map(mapper);
         return this.reverse ? changed.reverse().join('') : changed.join('');
     }
 }
 
 module.exports = VigenereCipheringMachine;
-
-    // process(message, key, mapper) {
-    //     this.pointer = 0; ?@!%//
-    //     if (message == undefined || key == undefined) {
-    //         throw new Error();
-    //     }
-    //     key = key.toUpperCase();
-    //     let changed = message.toUpperCase().split('').map(mapper);
-    //     return this.reverse ? changed.reverse().join('') : changed.join('');
-    // }
-
